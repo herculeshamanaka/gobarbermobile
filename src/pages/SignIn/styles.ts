55d5414bf367030eceac114a3cf42d168dfc9187
Hercules Neves Hamanaka <hamanhrc@schaeffler.com>
2020-05-10 11:45:20 -0300

    chore: Initial commit

---
+++ src/pages/SignIn/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+  font-variant: small-caps;
+`;
+
+export { Container, Title };
---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+const Container = styled.View``;
+
+export { Container };
