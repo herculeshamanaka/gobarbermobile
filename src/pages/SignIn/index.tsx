3c689cbf6facd3fc85bf62b7e11ed54e395689fd
Hercules Neves Hamanaka <hamanhrc@schaeffler.com>
2020-05-10 12:51:33 -0300

    feature: SignIn screen layout finished

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,31 +1,69 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
   return (
-    <Container>
-      <Image source={logoImg} />
-
-      <Title>Do your logon</Title>
-
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Password" />
-
-      <Button
-        onPress={() => {
-          console.log('pressed');
-        }}
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
       >
-        Logon
-      </Button>
-    </Container>
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Do your logon</Title>
+            </View>
+
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Password" />
+
+            <Button
+              onPress={() => {
+                console.log('pressed');
+              }}
+            >
+              Logon
+            </Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Forgot your password ?</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => {}}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Create Account</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
   );
 };
 
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,13 +1,14 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-const Container = styled.View`
+export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
   padding: 0 30px;
 `;
 
-const Title = styled.Text`
+export const Title = styled.Text`
   font-size: 24px;
   color: #f4ede8;
   font-family: 'RobotoSlab-Medium';
@@ -15,4 +16,34 @@ const Title = styled.Text`
   font-variant: small-caps;
 `;
 
-export { Container, Title };
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
+`;
